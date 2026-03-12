7e1a83adbb6d4ef9b642eb5a8f1e05fee978ba49
Chafahperez <fonsohperez32@gmail.com>
2026-03-12 13:19:48 +0100

    Add files via upload

---
+++ New folder/contact.php
@@ -0,0 +1,18 @@
+<?php
+
+include("../database/config.php");
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$sql = "INSERT INTO contacts(name,email,message)
+VALUES('$name','$email','$message')";
+
+if(mysqli_query($conn,$sql)){
+echo "Message Sent Successfully";
+}else{
+echo "Error sending message";
+}
+
+?>
